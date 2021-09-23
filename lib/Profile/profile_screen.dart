a8153bbcc5918b17bbb2ba2160d72e8121b63ff8
Song <pypvjpk@gmail.com>
2021-09-23 11:50:50 +0700

    [UPDATE] Profile
    change color appbar

--- lib/Profile/profile_screen.dart
+++ lib/Profile/profile_screen.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:himod/Profile/component/body_profile.dart';
 
-
 class ProfileScreen extends StatefulWidget {
   ProfileScreen({Key key}) : super(key: key);
 
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
-  
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
@@ -16,7 +14,18 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("Profile")
+        title: Text("Profile"),
+        flexibleSpace: Container(
+          decoration: new BoxDecoration(
+            gradient: new LinearGradient(
+              colors: [
+                const Color(0xffff9e23),
+                const Color(0xffff711b),
+                const Color(0xffff4814),
+              ],
+            ),
+          ),
+        ),
       ),
       body: BodyProfile(),
     );
