54990e599c1751ca57e8afec2337f426e31879bb
BIW <thanapat.bew1031@gmail.com>
2021-10-11 16:29:16 +0700

    [ADD] ViewpagePost & CumtomViewPost_post

---
+++ lib/viewpost_post.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ViewPost extends StatefulWidget {
+  final String uid;
+  final String postid;
+
+  ViewPost({Key key, this.uid, this.postid}) : super(key: key);
+
+  @override
+  _ViewPostState createState() => _ViewPostState();
+}
+
+class _ViewPostState extends State<ViewPost> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      
+    );
+  }
+}
