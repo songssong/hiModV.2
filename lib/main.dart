00762ccce0c81458460457f3da34e0fb4399f571
BIW <thanapat.bew1031@gmail.com>
2021-11-18 00:51:45 +0700

    [UPDATE] main page, pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
-import 'package:firebase_auth/firebase_auth.dart'; 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:himod/Comment/comment_screen.dart';
+import 'package:himod/LostAndFound/lostandfound_screen.dart';
 import 'package:himod/Profile/profile_screen.dart';
-import 'package:himod/activity.dart';
+import 'package:himod/Activity/activity_screen.dart';
+import 'package:himod/Activity/activity_detail.dart';
+import 'package:himod/homepage.dart';
 import 'package:himod/loadapp.dart';
-import 'package:himod/lostnfound.dart';
 import 'package:himod/lostnfounddetail.dart';
 import 'package:himod/post.dart';
 import 'package:himod/postdetail.dart';
@@ -14,43 +15,42 @@ import 'package:himod/providers/comment_provider.dart';
 import 'package:himod/signup.dart';
 import 'package:provider/provider.dart';
 
-
-
 String initialRoute = '/loginload';
 
 Future<Null> main() async {
   //เช็คการเข้าถึงว่า อุปกรณ์นี้เคยมีการ login ไว้แล้วไหม ถ้ามีการ login ไว้แล้วจะส่งไปที่หน้า post ทันที แต่ถ้าไม่จะใช้ค่าเดิมของ initialRoute
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp().then((value) async {
-    await FirebaseAuth.instance.authStateChanges().listen((event) { 
+    await FirebaseAuth.instance.authStateChanges().listen((event) {
       if (event != null) {
-        initialRoute='/lostnfounddetail';
+        initialRoute = '/homepage';
       }
       runApp(MyApp());
     });
   });
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-            create: (context) => CommentProvider(),
-          child: MaterialApp(
+      create: (context) => CommentProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
             primaryColor: Colors.white, scaffoldBackgroundColor: Colors.white),
         initialRoute: initialRoute,
         routes: {
-          '/loginload':(context) => Loginload(),
-          '/post':(context) => Post(),
-          '/profile':(context) => ProfileScreen(),
-          '/activity':(context) => Activity(),
-          // '/comment':(context) => Comment(),
-          '/comment':(context) => CommentScreen(),
-          '/postdetail':(context) => Postdetail(),
-          '/signup':(context) => Signup(),
-          '/lostnfound':(context) => Lostnfound(),
-          '/lostnfounddetail':(context) => lostnfounddetail(),
-          
+          '/loginload': (context) => Loginload(),
+          '/post': (context) => Post(),
+          '/profile': (context) => ProfileScreen(),
+          '/activity': (context) => Activity(),
+          '/postdetail': (context) => Postdetail(),
+          '/signup': (context) => Signup(),
+          '/lostnfound': (context) => LostAndFoundScreen(),
+          '/lostnfounddetail': (context) => lostnfounddetail(),
+          '/homepage': (context) => HomePage(),
+          '/activitydetail': (context) => Activitydetail(),
         },
         builder: EasyLoading.init(),
       ),
